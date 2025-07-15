28710c1ce3459a47da54937cb710993e78bea79c
Sadik Khan Pathan <PathanSadikkhan333@gmail.com>
2025-07-15 23:24:59 +0530

    added login system using email/username and password with session setup

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -40,23 +40,34 @@ export const authOptions: NextAuthOptions = {
         } catch (err: any) {
           throw new Error(err);
         }
-        },
+      },
     }),
   ],
-  pages: {
-    signIn:'/sign-in'
-  },
-  callbacks:{
-    async session({ session, token }) {
-      return session
-    },
+  callbacks: {
     async jwt({ token, user }) {
-      return token
-    }
+      if (user) {
+        token._id = user._id?.toString(); // Convert ObjectId to string
+        token.isVerified = user.isVerified;
+        token.isAcceptingMessages = user.isAcceptingMessages;
+        token.username = user.username;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user._id = token._id;
+        session.user.isVerified = token.isVerified;
+        session.user.isAcceptingMessages = token.isAcceptingMessages;
+        session.user.username = token.username;
+      }
+      return session;
+    },
   },
-  session:{
-    strategy:"jwt"
+  session: {
+    strategy: 'jwt',
   },
-  secret:
-     process.env.NEXTAUTH_SECRET
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: '/sign-in',
+  },
 };

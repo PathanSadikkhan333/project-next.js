5abad4d15c04c5826591c589e5597a960a4f8b32
pathan sadik khan <sadikkhanpathan09@gmail.com>
2025-07-17 11:02:08 +0530

    add basic authentication UI with sign in and sign out using NextAuth

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
-
-function page() {
-  return (
-    <div>
-      page
-    </div>
-  )
+'use client'
+import{useSession, signIn, signOut} from "next-auth/react"
+export default function Component(){
+const {data: session}=useSession()
+if(session) {
+return(
+<>
+signed in as {session. user. email}<br/>
+<button onclick={()=>signOut()}>sing out</button>
+</>
+)
 }
-
-export default page
+return(
+<>
+not signed in <br/>
+<button className="bg-orange-500 px-3 py-1 m-4 rounded"
+onclick={()=>signIn()}>sign in</button>
+</>
+)
+}
